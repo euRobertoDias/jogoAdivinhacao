e84766582dd7b765c2ef217640d17326da582f9d
euRobertoDias <hombrous@gmail.com>
2023-09-07 07:33:41 -0300

    jogo da adivinhação, bug - resete do botão e css da screenTwo

---
+++ main.js
@@ -0,0 +1,43 @@
+const screenOne = document.querySelector(".screen-one")
+const screenTwo = document.querySelector(".screen-two")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+let randomNumber = Math.round(Math.random() * 10)
+let xAttemps = 1
+
+// Events
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", function (e) {
+    if (e.key == "Enter" && screenOne.classList.contains("hide")) {
+        handleResetClick()
+    }
+})
+
+// Callback function
+function handleTryClick(event) {
+    event.preventDefault()
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if (Number(inputNumber.value) == randomNumber) {
+        handleToggleScreen()
+        screenTwo.innerText = `Você acertou em ${xAttemps} tentativas`
+    }
+
+    inputNumber.value = ""
+    xAttemps++
+}
+
+function handleResetClick() {
+    handleToggleScreen()
+    xAttemps = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function handleToggleScreen() {
+    screenOne.classList.toggle("hide")
+    screenTwo.classList.toggle("hide")
+}
+
+
