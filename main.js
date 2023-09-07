13e9ecea70859d82e31005da88b73f13439dd56f
euRobertoDias <hombrous@gmail.com>
2023-09-07 11:41:27 -0300

    incluído a regra para impedir tentativas vazias, abaixo de 0 e acima de 10

--- main.js
+++ main.js
@@ -10,13 +10,7 @@ let xAttemps = 1
 btnTry.addEventListener("click", handleTryClick)
 btnReset.addEventListener("click", handleResetClick)
 
-document.addEventListener("keydown", function (e) {
-
-    if (e.key == "Enter" && screenOne.classList.contains("hide")) {
-        handleResetClick()
-    }
-
-})
+document.addEventListener("keydown", handleResetEnter)
 
 
 // Callback function
@@ -24,11 +18,21 @@ function handleTryClick(event) {
     event.preventDefault()
     const inputNumber = document.querySelector("#inputNumber")
 
+    if (inputNumber.value == "") {
+        alert("Tentativa inválida")
+    }
+
+    if (Number(inputNumber.value) < 0) {
+        alert("O número é menor que 0, por favor tente um número entre 0 e 10")
+    }
+
+    if (Number(inputNumber.value) > 10) {
+        alert("O número é maior que 10, por favor tente um número entre 0 e 10")
+    }
+
     if (Number(inputNumber.value) == randomNumber) {
         handleToggleScreen()
         screenTwo.querySelector("h2").innerText = `Você acertou em ${xAttemps} tentativas`
-
-
     }
 
     inputNumber.value = ""
@@ -39,6 +43,12 @@ function handleResetClick() {
     handleToggleScreen()
     xAttemps = 1
     randomNumber = Math.round(Math.random() * 10)
+}
+
+function handleResetEnter(e) {
+    if (e.key == "Enter" && screenOne.classList.contains("hide")) {
+        handleResetClick()
+    }
 }
 
 function handleToggleScreen() {
